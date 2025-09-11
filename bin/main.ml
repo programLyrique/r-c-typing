319a9e0d2a3767e1913561e10136b188eb5dfa9f
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2025-09-11 18:46:32 +0200

    Some scaffolding

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = Printf.printf "Hello world!\n"
+let main _ = 
+  if Array.length Sys.argv <> 2 then
+    Printf.eprintf "Usage: %s <c_file>\n" Sys.argv.(0)
+  else
+    let filename = Sys.argv.(1) in
+    let ast = R_c_typing.Parser.parse_file filename in
+    ()
+    
+let () = main ()
