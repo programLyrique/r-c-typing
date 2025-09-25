26a680c27a882901c75d70cbe633a0b149137c84
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2025-09-25 17:10:48 +0200

    Add command line options using cmdliner.

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,36 @@
 open R_c_typing
+open Cmdliner
 
-let main _ = 
-  if Array.length Sys.argv <> 2 then
-    Printf.eprintf "Usage: %s <c_file>\n" Sys.argv.(0)
-  else
-    let filename = Sys.argv.(1) in
-    (* Add optional call to pre-processor?
-      cpp -E -I ~/Documents/RLanguage/R-4.5.0/src/include test/cprogs/small.c -o test/cprogs/small-p.c
-    *)
+
+let main cst ast filename =
+  if cst then
     let cst = Parser.parse_file filename in
-    Parser.print_res cst;
-    Printf.printf "%s\n" (Ast.show_definitions (Parser.to_ast cst));
-    ()
-    
+    Parser.print_res cst
+  else if ast then
+    let cst = Parser.parse_file filename in
+    Printf.printf "%s\n" (Ast.show_definitions (Parser.to_ast cst))
+  else
+    Printf.eprintf "Usage: %s [--cst|--ast] <c_file>\n" Sys.argv.(0)
 
-let () = main ()
+let cst_opt =
+  let doc = "Print CST (concrete syntax tree)" in
+  Arg.(value & flag & info ["cst"] ~doc)
+
+let ast_opt =
+  let doc = "Print AST (abstract syntax tree)" in
+  Arg.(value & flag & info ["ast"] ~doc)
+
+let file_arg =
+  let doc = "C source file to parse" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+
+let cmd =
+  let open Term.Syntax in
+  Cmd.v
+    (Cmd.info "r-c-typing")
+    (let+ cst = cst_opt
+     and+ ast = ast_opt
+     and+ filename = file_arg in
+     main cst ast filename)
+
+let () = exit (Cmd.eval cmd)
