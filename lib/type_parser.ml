4d1f55df187331199ed20b5ad1b75f88eed92912
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2025-12-17 18:12:28 +0100

    Experiment with the new type algebra

--- lib/type_parser.ml
+++ lib/type_parser.ml
@@ -6,16 +6,16 @@ A line is: symbol: type
 
 The type itself is parsed using rstt
 *)
-(* open Mlsem.Types *)
 open Rstt
 
 let parse_type_line line = 
-  match String.split_on_char ':' line with 
-  | [sym; ty_str] -> 
+  match Utils.split_once ':' line with 
+  | (_, "") -> 
+      Printf.eprintf "Warning: could not parse line (missing type): %s@." line; None
+  | (sym, ty_str) -> 
       let ty_str = String.trim ty_str in 
       let ty = Rstt_repl.IO.parse_type ty_str in 
       Some (String.trim sym, ty)
-  | _ -> Printf.eprintf "Warning: could not parse line: %s@." line; None
 
 
 let parse_type_file filename = 
@@ -25,7 +25,19 @@ let parse_type_file filename =
   List.filter_map parse_type_line contents
 
 
+let build_types ti_map env type_list = 
+  List.fold_left (fun acc (sym, ty) ->
+    let open Builder in 
+    let (ty_env, env) = acc in 
+      let env,ty = Builder.resolve env ty in
+      let ty = build ti_map ty in
+      (StrMap.add sym ty ty_env, env)
+    ) (Builder.StrMap.empty, env) type_list
 
+
+let mk_arg l =
+  let open Builder in
+  TArg { pos = []; pos_named =l; tl = TOption TEmpty; named = [] }
 
 let%test "parse simple types" =
   let line = "x: v(int)" in
@@ -35,12 +47,40 @@ let%test "parse simple types" =
   | None -> false
 
 let%test "parse arrow types" =
-  let line = "f: (v(int)) -> v(dbl)" in
+  let line = "f: (a:v(int)) -> v(dbl)" in
   match parse_type_line line with
   | Some (sym, ty) ->
-      (* let _,vty = Builder.resolve Builder.empty_env ty in
-      let ti_map = Builder.TIdMap.empty in
-      let vty = Builder.build ti_map vty in
-      Format.printf "Parsed type: %a@." Rstt.Pp.ty vty; *)
-      sym = "f" && Builder.(TArrow (TVec PInt, TVec PDbl)) = ty
+      sym = "f" && Builder.(TArrow (mk_arg [("a", TVec PInt)], TVec PDbl)) = ty
   | None -> false
+
+let%test "parse several types" =
+  let lines = [
+    "x: v(int)";
+    "y: v(dbl)";
+    "f: (a: v(int)) -> v(dbl)";
+  ] in
+  let results = List.map parse_type_line lines in
+  match results with
+  | [Some (s1,t1); Some (s2,t2); Some (s3,t3)] ->
+      s1 = "x" && Builder.(TVec PInt) = t1 &&
+      s2 = "y" && Builder.(TVec PDbl) = t2 &&
+      s3 = "f" && Builder.(TArrow (mk_arg [("a", TVec PInt)], TVec PDbl)) = t3
+  | _ -> false
+
+let%test "build types" =
+let open Builder in 
+  let lines = [
+    "x: v(int)";
+    "y: v(dbl)"
+  ] in
+  let parsed_types = List.filter_map parse_type_line lines in
+  let ti_map = TIdMap.empty in
+  let type_map, _ = build_types ti_map empty_env parsed_types in
+  (* print types *)
+  (* Format.printf "Types:@.";
+  StrMap.iter (fun sym ty ->
+    Format.printf "  %s: %a@." sym Rstt.Pp.ty ty
+  ) type_map ; *)
+  let open Rstt in
+  Ty.equiv (StrMap.find "x" type_map) (Prim.Int.any |> Prim.mk |> Vec.mk)  &&
+  Ty.equiv (StrMap.find "y" type_map) (Prim.Dbl.any |> Prim.mk |> Vec.mk)
--- lib/utils.ml
+++ lib/utils.ml
@@ -2,3 +2,11 @@ let is_singleton l =
   match l with
   | [_] -> true
   | _ -> false
+
+let split_once sep s =
+  try
+    let i = String.index s sep in
+    let first = String.sub s 0 i in
+    let rest = String.sub s (i + 1) (String.length s - i - 1) in
+    (first, rest)
+  with Not_found -> (s, "")
